4c9fec5ee578334010968851823c8297c72ebd05
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-10-04 14:33:13 -0400

    exercise complete

--- vampire.js
+++ vampire.js
@@ -10,21 +10,36 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else {
+      return false;
+    }
 
   }
 
@@ -35,8 +50,42 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
+  closestCommonAncestor(vampire) { 
+    let vmpA = this;
+    let vmpB = vampire;
 
+    // check if the same vampire
+    if (vmpA === vmpB) {
+      return vmpA;
+    }
+
+    // check if direct ancestor is used
+    if (vmpA.creator === vmpB) {
+      return vmpB;
+    } else if (vmpB.creator === vmpA) {
+      return vmpA;
+    }
+
+    while (vmpA.creator || vmpB.creator) {
+      if (vmpA.creator !== vmpB.creator) {
+        
+        if (vmpA.numberOfVampiresFromOriginal > vmpB.numberOfVampiresFromOriginal) {
+          vmpA = vmpA.creator;
+        } else {
+          vmpB = vmpB.creator;
+        }
+
+      } else {
+        if (vmpA.isMoreSeniorThan(vmpB)) {
+          return vmpA.creator;
+
+        } else {
+          return vmpB.creator;
+          
+        }
+      }
+    }
+    return vmpA;
   }
 }
 
